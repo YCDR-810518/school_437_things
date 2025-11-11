42cbf663110ae538c8f2cd01a9bb3c025738f710
YCDR-810518 <yaochen1234567@outlook.com>
2025-11-11 09:35:18 +0800

    hjc@update

---
+++ randomTwoColorBall/randomTwoColorBall/randomTwoColorBall/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  randomTwoColorBall
+//
+//  Created by 姚晨 on 2025/11/11.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
